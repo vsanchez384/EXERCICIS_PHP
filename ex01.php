ccbe6d79c78e057f2786a4b0b8e34c720be6da7f
vsanchez384 <vsanchez384@alumnes.politecnicllevant.cat>
2024-01-11 15:37:52 +0100

    commit

--- ex01.php
+++
@@ -1,8 +0,0 @@
-<html>
-    <?php
-    function saludar()
-    {
-        echo "{$this->nom} ({$this->dni} - {$this->dataNaixement} - {$this->genere}):"
-    }
-    ?>
-</html>
--- index.php
+++ index.php
@@ -1,14 +1,8 @@
 <html>
     <?php
-    echo "<h1>EXERCICIS POO</h1>";
+    echo "<h1>FORMULARIS</h1>";
 
-    # EXERCICI 1
-    echo "<br>EXERCICI 1<br>" . "<br>";
-
-    # EXERCICI 2
-    echo "<br>EXERCICI 2<br>" . "<br>";
-
-    # EXERCICI 3
-    echo "<br>EXERCICI 3<br>" . "<br>";
+    # EXEMPLE DE FORMULARI
+    
     ?>
 </html>
