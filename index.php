0a7ca6d9f5e8d37f674e4b8c8d1a5977be0db9ce
vsanchez384 <vsanchez384@alumnes.politecnicllevant.cat>
2024-01-11 15:31:39 +0100

    commit

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
 <html>
     <?php
-    
+    echo "<h1>EXERCICIS POO</h1>";
+
+    # EXERCICI 1
+    echo "<br>EXERCICI 1<br>" . "<br>";
+
+    # EXERCICI 2
+    echo "<br>EXERCICI 2<br>" . "<br>";
+
+    # EXERCICI 3
+    echo "<br>EXERCICI 3<br>" . "<br>";
     ?>
 </html>
