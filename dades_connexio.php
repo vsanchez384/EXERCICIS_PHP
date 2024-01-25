fff7726bead91a6e2ae83200b2ac34b2bce44179
vsanchez384 <vsanchez384@alumnes.politecnicllevant.cat>
2024-01-25 16:41:12 +0100

    commit

--- dades_connexio.php
+++ dades_connexio.php
@@ -8,7 +8,7 @@
      $conn = new PDO("mysql:host=$servername;dbname=prova", $username, $password);
      // set the PDO error mode to exception
      $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-     echo "Connected successfully";
+     echo "T'has connectat correctament <br>";
    } catch(PDOException $e) {
      echo "Connection failed2: " . $e->getMessage();
    }
--- index.php
+++ index.php
@@ -22,21 +22,51 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
         $sql_insert = "INSERT INTO CLIENT (nom, llinatge1, llinatge2, email) VALUES ('$name', '$llinatge1', '$llinatge2', '$email')";
         $conn->exec($sql_insert);
 
-        echo "Nou registre creat amb èxit";
+        echo "Nou registre creat amb èxit. <br>";
         $last_id = $conn->lastInsertId();
-        echo " La darrera ID insertada és: " . $last_id;
+        echo " La darrera ID insertada és: " . $last_id . "<br>";
     } catch (PDOException $e) {
         echo $sql_insert . "<br>" . $e->getMessage();
     }
 }
 ?>
-
-<form method="get">
+<br>
+<form method="get" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
     Nom: <input type="text" name="name"><br>
     Llinatge1: <input type="text" name="llinatge1"><br>
     Llinatge2: <input type="text" name="llinatge2"><br>
     Email: <input type="text" name="email"><br>
     <input type="submit" value="Enviar">
 </form>
+
+<?php
+try {
+    $stmt = $conn->prepare("SELECT * FROM CLIENT");
+    $stmt->execute();
+    $conn = null;
+} catch (PDOException $e) {
+    echo "Error: " . $e->getMessage();
+}
+$arrayValues = $stmt->fetchAll(PDO::FETCH_ASSOC);
+echo "<table wdith=\"100%\">\n";
+echo "<tr>\n";
+// add the table headers
+foreach ($arrayValues[0] as $key => $useless) {
+    echo "<th>$key</th>";
+}
+echo "</tr>";
+// display data
+foreach ($arrayValues as $row) {
+    echo "<tr>";
+    foreach ($row as $key => $val) {
+        echo "<td>$val</td>";
+    }
+    echo "</tr>\n";
+}
+// close the table
+echo "</table>\n";
+
+
+?>
 
 </html>
